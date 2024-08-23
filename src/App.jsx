e59a8e7fa1beb9418c3d5f1dd9a5e2507650d7a5
Armond-Guze <Armond.Guze@yahoo.com>
2024-08-23 00:48:54 -0400

    start banner headings

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import Navbar from "./Components/Navbar";
-import ExersizeDetail from "./pages/ExersizeDetail";
+import ExersiceDetail from "./pages/ExersizeDetail";
 import Home from "./pages/Home";
 import Footer from "./Components/Footer";
+import Exersices from "./Components/Exersices";
+import Hero from "./Components/Hero";
+import SearchExercises from "./Components/SearchExercises";
 
 const App = () => {
   return (
     <Router>
-      <Box width="400px" sx={{ width: { xl: '1488px' }}} m='auto'>
+      <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/exercize/:id" element={<ExersizeDetail />} />
+          <Route path="/exercise/:id" element={<ExersiceDetail />} />
         </Routes>
         <Footer />
       </Box>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../Components/Hero";
+import SearchExercises from "../Components/SearchExercises";
+import Exersices from "../Components/Exersices";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exersices />
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
