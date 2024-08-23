b2cda18e684314efdeb7c09531cba16d8fe9e938
Armond-Guze <Armond.Guze@yahoo.com>
2024-08-22 23:51:04 -0400

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import Navbar from "./Components/Navbar";
+import ExersizeDetail from "./pages/ExersizeDetail";
+import Home from "./pages/Home";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Box width="400px" sx={{ width: { xl: '1488px' }}} m='auto'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercize/:id" element={<ExersizeDetail />} />
+        </Routes>
+        <Footer />
+      </Box>
+    </Router>
+  );
+};
 
-export default App
+export default App;
